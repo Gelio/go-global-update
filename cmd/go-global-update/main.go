4cccaf55935c67bcef21a869bd8b9e481e604d6f
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-09-04 12:37:44 +0200

    handle detecting GOBIN based on `go env GOBIN` and `go env GOPATH`
    Handles more cases, as setting real environment variables is not
    required. This makes it so that the package only relies on the same
    mechanism that go uses to resolve those directories.

--- cmd/go-global-update/main.go
+++ cmd/go-global-update/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -9,10 +10,37 @@ import (
 	"strings"
 )
 
+func get_executable_binaries_path() (string, error) {
+	cmd := exec.Command("go", "env", "GOBIN")
+	gobin_bytes, err := cmd.Output()
+	if err != nil {
+		return "", nil
+	}
+	gobin := strings.TrimSpace(string(gobin_bytes))
+
+	if len(gobin) > 0 {
+		return gobin, nil
+	}
+
+	cmd = exec.Command("go", "env", "GOPATH")
+	gopath_bytes, err := cmd.Output()
+	if err != nil {
+		return "", nil
+	}
+	gopath := strings.TrimSpace(string(gopath_bytes))
+	if len(gopath) == 0 {
+		return "", errors.New("GOPATH and GOPATH are not defined in 'go env' command")
+	}
+
+	gobin = fmt.Sprintf("%s/%s", gopath, "bin")
+
+	return gobin, nil
+}
+
 func main() {
-	gobin := os.Getenv("GOBIN")
-	if len(gobin) == 0 {
-		fmt.Println("GOBIN environment variable is not defined")
+	gobin, err := get_executable_binaries_path()
+	if err != nil {
+		fmt.Println("Error while trying to determine the executable binaries path", err)
 		os.Exit(1)
 	}
 
@@ -50,15 +78,15 @@ func main() {
 			matched = true
 
 			binaryPath := matches[1]
-			cmd = exec.Command("go", "get", "-u", binaryPath)
-			stdout, err = cmd.Output()
+			cmd = exec.Command("go", "install", fmt.Sprintf("%s@latest", binaryPath))
+			stdout, err = cmd.CombinedOutput()
 
 			if err != nil {
 				fmt.Println("Error when updating package", binaryPath, ", path", binaryPath)
 				os.Exit(6)
 			}
 			fmt.Println("Upgraded package", binaryName)
-			fmt.Print(stdout)
+			fmt.Println(string(stdout))
 		}
 		if !matched {
 			fmt.Println("Output for package", binaryName, "does not contain a \"path\" section")
