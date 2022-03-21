1f4568886e654947f4834b385c0f4f125826887e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-03-21 21:56:49 +0100

    test: ensure known problem URL hashes are correct
    Add a test verifying that the URL hashes of common problems match the
    hashes in the table of contents of TROUBLESHOOTING.md.

--- internal/updater/common_problems_test.go
+++ internal/updater/common_problems_test.go
@@ -1,9 +1,15 @@
 package updater
 
 import (
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFindCommonProblems(t *testing.T) {
@@ -69,5 +75,39 @@ go install: command-line-arguments@latest: malformed module path "command-line-a
 
 			assert.ElementsMatch(t, problemNames, c.expectedProblemNames, "mismatched errors found (listA) while expected listB")
 		})
+	}
+}
+
+func TestCommonProblemHashes(t *testing.T) {
+	workingDirectory, err := os.Getwd()
+	require.Nil(t, err)
+	troubleshootingFilePath := filepath.Join(workingDirectory, "..", "..", "TROUBLESHOOTING.md")
+	troubleshootingFileBuffer, err := os.ReadFile(troubleshootingFilePath)
+	require.Nil(t, err)
+	troubleshootingFileContents := string(troubleshootingFileBuffer)
+
+	hashRegexp := regexp.MustCompile(`^- \[.*\]\((.*)\)$`)
+	var knownProblemHashes []string
+	endOfLineSequence := "\n"
+	if runtime.GOOS == "windows" {
+		endOfLineSequence = "\r\n"
+	}
+
+	for _, line := range strings.Split(troubleshootingFileContents, endOfLineSequence) {
+		matches := hashRegexp.FindStringSubmatch(line)
+		if matches != nil {
+			assert.Len(t, matches, 2, "hashRegexp matched an unexpected line")
+			knownProblemHashes = append(knownProblemHashes, matches[1])
+		}
+	}
+
+	require.GreaterOrEqualf(t, len(knownProblemHashes), 4,
+		"could not find hashes in %s\nfile contents: %s",
+		troubleshootingFilePath, troubleshootingFileContents)
+
+	allCommonProblems := append(commonUpdateProblems, binaryBuiltFromSourceProblem)
+	for _, p := range allCommonProblems {
+		assert.Containsf(t, knownProblemHashes, p.troubleshootingHeadingHash,
+			"not a known hash. Check if there is a typo in the heading hash for problem %v", p)
 	}
 }
